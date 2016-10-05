b12d9bed2f0b86fa24504a7cfb8c5260168ace78
Dimitris Sarlis <sarlis.dim@gmail.com>
2016-10-04 18:04:41 -0700

    Added function to parse port

--- app.js
+++ app.js
@@ -12,3 +12,20 @@ app.get('/', function (req, res) {
 app.listen(port, function () {
 		  console.log('Example app listening on port ' + port);
 });
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
