e18686955b646474a804f552fc53517dc6ecc8be
Anton Sergeyev <sergeyev.anton.s@gmail.com>
2017-10-04 12:40:29 -0700

    super cool feature

--- src/Game.js
+++ src/Game.js
@@ -4,7 +4,7 @@ function Game(){
     this._frames = [];
     this._bonusThrow = 0;
     this._bonusThrows = [];
-    this._gameSize = 10;
+    this._gameSize = 5;
 };
 
 Game.prototype.addFrame = function(frame){
